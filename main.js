cf91f410a09e5aac3684d3edfcc59b81596b3ae2
rayinabl <rayinabl@gmail.com>
2023-11-16 22:57:57 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// change color navbar
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+// show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close nav menu
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+ 
+closeBtn.addEventListener('click', closeNav);
+
+// swipper image
+var swiper = new Swiper(".mySwiper", {
+    slidePerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickKable: true,
+    },
+})
+
+
